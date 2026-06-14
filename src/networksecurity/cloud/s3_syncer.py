cdc5e68474563175bc0208eac5594dc97d467fde
SankalpMakol016 <sankalp.sumit.makol@gmail.com>
2026-06-14 23:56:43 +0530

    Implement AWS S3 sync functionality

--- src/networksecurity/cloud/s3_syncer.py
+++ src/networksecurity/cloud/s3_syncer.py
@@ -1,6 +1,25 @@
+import subprocess
+
+
 class S3Syncer:
     def sync_folder_to_s3(self, folder_path: str, bucket_name: str, s3_path: str) -> None:
+        command = [
+            "aws",
+            "s3",
+            "sync",
+            folder_path,
+            f"s3://{bucket_name}/{s3_path}"
+        ]
+        subprocess.run(command, check=True)
         print(f"Syncing {folder_path} to s3://{bucket_name}/{s3_path}")
 
     def sync_folder_from_s3(self, folder_path: str, bucket_name: str, s3_path: str) -> None:
+        command = [
+            "aws",
+            "s3",
+            "sync",
+            f"s3://{bucket_name}/{s3_path}",
+            folder_path
+        ]
+        subprocess.run(command, check=True)
         print(f"Syncing s3://{bucket_name}/{s3_path} to {folder_path}")
